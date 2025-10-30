2f7dc4702067a17df163deea28f1f2ddad3175dd
Abdul Rehman <abdulz@Abduls-MacBook-Air.local>
2025-10-29 20:02:30 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, session, redirect
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        if email == "a@m.com" and password == "1":
+            return redirect('main.html')
+
+
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
